7483d3c07e0f2bd4cf30bb1fc1494fbe30f8efc4
Frederick Roegiers <frederick@rogerthat.be>
2025-10-15 12:12:41 +0200

    wip

--- interface/js/app.js
+++ interface/js/app.js
@@ -2,3 +2,12 @@
 const $btnOn = document.querySelector("#on");
 const $btnOff = document.querySelector("#off");
 const $ulMessages = document.querySelector("#messages");
+
+// broker data
+const broker = "192.168.1.114";
+const port = 9001;
+const topic = "technology/arduino/demo";
+const url = "ws://" + broker + ":" + port;
+
+// mqtt client aanmaken
+const cutie = mqtt.connect(url);
